a62dcc3b3283fb22aff1046aca7dad1fec74ca22
amo-tech <kaomi.x1003@gmail.com>
2022-10-26 07:48:43 +0900

    component

--- src/index.js
+++ src/index.js
@@ -1,14 +1,6 @@
 import ReactDom from "react-dom";
+import App from "./App";
 
-const App = () => {
-  return (
-    // React.Fragment
-    <>
-      <h1>Hello!</h1>
-      <p>How are you</p>
-    </>
-  );
-};
 
-// ReactDom.render(<App />, document.getElementById("root"));
-createRoot.render(<App />, document.getElementById("root"));
+ReactDom.render(<App />, document.getElementById("root"));
+// createRoot.render(<App />, document.getElementById("root"));
