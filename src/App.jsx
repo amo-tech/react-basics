0402dc1455d6362ba893204bc3b05e2bf63b3106
amo-tech <kaomi.x1003@gmail.com>
2022-10-29 18:54:19 +0900

    state

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import ColorfulMassage from "./components/ColorfulMessage";
 
 const App = () => {
-    const onClickButton = () => {
-      alert();
+    const onClickCoutUp = () => {
+      setNum(num + 1);
     }
-    
+    const [num, setNum] = useState(0);
     return (
         <>
       <h1 style={{ color: "red" }}>Hello!</h1>
       <ColorfulMassage color="blue">How are you</ColorfulMassage>
       <ColorfulMassage color="pink">Good</ColorfulMassage>
-      <button onClick = {onClickButton}>Button</button>
+      <button onClick = {onClickCoutUp}>CountUp</button>
+      <p>{num}</p>
     </>
     );
 };
