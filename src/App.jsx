ccd3b8d599aa049215db9db92b18a7e84f7d9881
amo-tech <kaomi.x1003@gmail.com>
2022-10-30 14:04:18 +0900

    useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,40 @@
-import React, {useState} from "react";
-import ColorfulMassage from "./components/ColorfulMessage";
+import React, { useEffect, useState } from "react";
+import { ColorfulMassage } from "./components/ColorfulMessage";
 
 const App = () => {
-    const onClickCoutUp = () => {
-      setNum(num + 1);
+// React= 状態+関数→クロージャー
+  const [num, setNum] = useState(0);
+  const [faceShowFlag, setFaceShowFlag] = useState(false);
+  const onClickCountUp = () => {
+    setNum(num + 1);
+  }
+  const onClickSwitchShowFlag =() => {
+    setFaceShowFlag(!faceShowFlag);
+  }
+
+  useEffect(() => {
+    if (num > 0 ) {
+      if (num % 3 === 0) {
+        faceShowFlag || setFaceShowFlag(true);
+      } else {
+        faceShowFlag && setFaceShowFlag(false);
+      }
     }
-    const [num, setNum] = useState(0);
-    return (
-        <>
+  }, [num]);
+  
+
+  return (
+    <>
       <h1 style={{ color: "red" }}>Hello!</h1>
       <ColorfulMassage color="blue">How are you</ColorfulMassage>
       <ColorfulMassage color="pink">Good</ColorfulMassage>
-      <button onClick = {onClickCoutUp}>CountUp</button>
+      <button onClick={onClickCountUp}>CountUp</button>
+      <br/>
+      <button onClick={onClickSwitchShowFlag}>on / off</button>
       <p>{num}</p>
+      {faceShowFlag && <p>(=^・・^=)</p>}
     </>
-    );
+  );
 };
 
 export default App;
--- src/components/ColorfulMessage.jsx
+++ src/components/ColorfulMessage.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 
 // 大文字？
-const ColorfulMassage = (props) => {
-    const { color, children} = props; // 分割代入
+export const ColorfulMassage = (props) => {
+    const { color, children } = props; // 分割代入
     const contentStyle = {
         color,
         fontSize: "18px",
-      }
+    }
     return <p style={contentStyle}>{children}</p> // children使う？
-}
-
-export default ColorfulMassage;
+};
